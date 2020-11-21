f1a4151a0d5a77f97fc2330cf5c4e9ed034ccd30
Natalman <natalnahm@gmail.com>
2020-11-21 17:22:49 -0600

    Add a adapter class to be used to display item on the screen.

---
+++ app/src/main/java/com/example/sqlite_project/EventCursorAdapter.java
@@ -0,0 +1,31 @@
+package com.example.sqlite_project;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class EventCursorAdapter extends CursorAdapter {
+    public EventCursorAdapter(Context context, Cursor c, boolean autoRequery) {
+        super(context, c, autoRequery);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView titleText = view.findViewById(R.id.event_title_text);
+        TextView dateText = view.findViewById(R.id.event_date_text);
+        ImageView img = view.findViewById(R.id.event_img_holder);
+
+        titleText.setText(cursor.getString(cursor.getColumnIndex(DBHelper.TITLE_COL)));
+        dateText.setText(cursor.getString(cursor.getColumnIndex(DBHelper.DATE_COL)));
+        img.setImageResource(cursor.getInt(cursor.getColumnIndex(DBHelper.IMAGE_ID_COL)));
+    }
+}
--- app/src/main/java/com/example/sqlite_project/MainActivity.java
+++ app/src/main/java/com/example/sqlite_project/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.sqlite_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -17,5 +19,19 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, CreateEventActivity.class);
         FloatingActionButton fab = findViewById(R.id.add_event_btn);
         fab.setOnClickListener((view) -> startActivity(intent));
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        DBHelper dbHelper = new DBHelper(this, DBHelper.DATABASE_NAME, null, 1);
+        SQLiteDatabase reader = dbHelper.getReadableDatabase();
+        String[] columns = {"_id", DBHelper.TITLE_COL, DBHelper.DATE_COL, DBHelper.IMAGE_ID_COL};
+        Cursor cursor = reader.query(DBHelper.TABLE_NAME, columns, null, null,
+                null, null, null);
+
+
     }
 }
